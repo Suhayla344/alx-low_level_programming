f964d278c86991bd794e887e9e6a423395d824eb
Suhayla344 <shylhk228@gmail.com>
2023-08-30 00:50:52 +0300

    ghj

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the node at a certain index in a linked list
+ * @head: first node in the linked list
+ * @index: index of the node to return
+ *
+ * Return: poin~~*/
