4c58d871914da79d0f9306d692ac1e759d13dc8d
Suhayla344 <shylhk228@gmail.com>
2023-10-12 19:21:11 +0300

    pass

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int ac, char **av);
 
 #endif
