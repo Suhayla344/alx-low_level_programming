00bc879bcf06d5b3bcd57337d60894e1d3c4a148
Suhayla344 <shylhk228@gmail.com>
2024-01-30 18:08:36 +0200

    edf

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
+
 int add(int a, int b) {
-	return a + b;
+	return (a + b);
 }
 int sub(int a, int b) {
-	return a - b;
+	return (a - b);
 }
 int mul(int a, int b) {
-	if (b == 0) {
-		printf("Error: Division by zero\n");
-		return 0;
-	}
-	return a / b;
+	return (a * b);
 }
 int mod(int a, int b) {
 	if (b == 0) {
 		printf("Error: Division by zero\n");
 		return 0;
 	}
-	return 0;
+	return (a % b);
+}
+int div(int a, int b) {
+	if (b == 0) {
+		printf("Erorr: Division by zero\n");
+		return (0);
+	}
+	return (a / b);
 }
